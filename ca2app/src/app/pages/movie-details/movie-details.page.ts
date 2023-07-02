146ffd7457c3ff0dc09826c4f48af5d5ee7c25b1
Om i <115377488+Om-i@users.noreply.github.com>
2023-07-02 16:24:59 +0000

    Details Content (ionicons and click button)

--- ca2app/src/app/pages/movie-details/movie-details.page.ts
+++ ca2app/src/app/pages/movie-details/movie-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -9,6 +10,7 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class MovieDetailsPage implements OnInit {
   movie: any = null;
+  imageBaseUrl = environment.images;
 
   constructor(private route: ActivatedRoute, private movieService: MovieService) { }
 
@@ -22,4 +24,7 @@ export class MovieDetailsPage implements OnInit {
     }
   }
 
+  openHomepage() {
+    window.open(this.movie.homepage)
+  }
 }
