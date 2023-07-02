23376cd085c6419c452321ec953ba06bfeaaff51
Om i <115377488+Om-i@users.noreply.github.com>
2023-07-02 13:34:21 +0000

    API implementation and 1st page

--- ca2app/src/app/services/movie.service.ts
+++ ca2app/src/app/services/movie.service.ts
@@ -3,6 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export interface ApiResult {
+  page: number;
+  results: any[];
+  total_pages: number;
+  total_results: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,10 +17,16 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  getTopRatedMovies(): Observable<any> {
-    return this.http.get(`${environment.baseUrl}movie/popular?api_key=${environment.apiKey}`); // note `` not ''
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(
+      `${environment.baseUrl}movie/popular?api_key=${environment.apiKey}&page=${page}` // note `` not '' as it accepts dynamic variables ${}
+      );
   }
 
-  getMovieDetails() {}
+  getMovieDetails(id: string) {
+    return this.http.get(
+      `${environment.baseUrl}movie/${id}?api_key=${environment.apiKey}`
+      );
+  }
 
 }
