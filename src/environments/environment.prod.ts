ff88f19ce99c2cd7faed954a74dd0647a5ed135e
Om i <115377488+Om-i@users.noreply.github.com>
2023-07-02 17:24:49 +0000

    added production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '14c66f2a042c7bc6b817f3ea612a7aa7',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
