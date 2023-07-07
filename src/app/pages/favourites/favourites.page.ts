18906ae6e5bb6a57d7c7e2add0d0ab2c9184b99d
Om i <115377488+Om-i@users.noreply.github.com>
2023-07-07 04:17:31 +0000

    Favourites List

--- src/app/pages/favourites/favourites.page.ts
+++ src/app/pages/favourites/favourites.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { finalize, forkJoin } from 'rxjs';
+import { FavouriteService } from 'src/app/services/favourite.service';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-favourites',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourites.page.scss'],
 })
 export class FavouritesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+    movies: any[] = [];
+    imageBaseUrl = environment.images;
+  
+    constructor(
+      private movieService: MovieService,
+      private loadingCtrl: LoadingController,
+      private favouriteService: FavouriteService
+    ) {}
+  
+    ngOnInit() {
+      this.loadMovies();
+    }
+  
+    async loadMovies() {
+      // this.movies = [];
+      const loading = await this.loadingCtrl.create({
+        message: 'Loading...',
+        spinner: 'bubbles',
+      });
+      await loading.present();
+  
+      const favorites = await this.favouriteService.getAllFavouriteFilms();
+      forkJoin<any[]>(favorites.map((id: any) => this.movieService.getMovieDetails(id)))
+        .pipe(finalize(() => loading.dismiss()))
+        .subscribe((movies) => {
+          this.movies.push(...movies);
+        });
+        console.log(this.movies);
+    }
   }
-
-}
